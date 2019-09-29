ec42e81b0c0b9dfed5510c1c225960e0e2d19226
Emerson Paiva <emersonaraujopaiva@gmail.com>
2019-09-29 19:21:05 -0300

    Create PostItem component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
+import PostItem from '../components/PostItem/PostItem';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+    <PostItem
+      slug="/about/"
+      background="gray"
+      category="JS"
+      date="23 de junho de 2019"
+      timeToRead="9"
+      title="E acabou o melhor ano da minha vida (até agora)"
+      description="Um pouco sobre as minhas conquistas em 2018 e as metas para 2019"
+    />
   </Layout>
 );
 
