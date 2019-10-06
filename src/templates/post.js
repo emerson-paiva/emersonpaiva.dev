ab6f5b7adc21d293117b0bb109c920856533f8a8
Emerson Paiva <emersonaraujopaiva@gmail.com>
2019-10-06 13:54:45 -0300

    Styled Post

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
+import Layout from '../components/Layout';
+import SEO from '../components/seo';
+
+import * as S from '../components/Post/styled';
+
 const Post = ({ data: { markdownRemark: post } }) => {
   return (
-    <>
-      <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
+      <S.PostHeader>
+        <S.PostDate>
+          {post.frontmatter.date} • {post.timeToRead} min de leitura
+        </S.PostDate>
+        <S.PostTitle>{post.frontmatter.title}</S.PostTitle>
+        <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
+      </S.PostHeader>
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </S.MainContent>
+    </Layout>
   );
 };
 
 export const query = graphql`
   query Post($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
-      html
       frontmatter {
-        category
-        background
-        date
         title
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+        description
       }
+      html
+      timeToRead
     }
   }
 `;
