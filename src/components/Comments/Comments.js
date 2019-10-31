19bd97ebba2ac513feea60d1354ac05e7fd15e82
Emerson Paiva <emersonaraujopaiva@gmail.com>
2019-10-31 10:03:11 -0300

    Update Comments.js

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -11,7 +11,7 @@ const Comments = ({ url, title }) => {
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
-        shortname="emerpaiva"
+        shortname="emersonpaiva"
         identifier={completeURL}
         title={title}
         url={completeURL}
