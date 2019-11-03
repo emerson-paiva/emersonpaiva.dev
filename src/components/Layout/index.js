bc54e34c29cc6deaf952f007c2d811be1e703215
Emerson Paiva <emersonaraujopaiva@gmail.com>
2019-11-03 12:34:37 -0200

    Adicionado dark mode

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Sidebar from '../Sidebar/Sidebar';
-import MenuBar from '../MenuBar/MenuBar';
+import MenuBar from '../MenuBar';
 
 import * as S from './styled';
 import GlobalStyles from '../../styles/global';
