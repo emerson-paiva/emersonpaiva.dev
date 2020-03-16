56ab9f0f7935da17ebfbddcb21c8b0319b1a1dc1
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-03-15 22:52:41 -0300

    add Title to FindMe items

--- src/components/core/FindMe/FindMe.js
+++ src/components/core/FindMe/FindMe.js
@@ -4,8 +4,8 @@ import { Linkedin } from '@styled-icons/boxicons-logos/Linkedin'
 
 import * as S from './FindMeStyle'
 
-const SocialMediaLink = ({ url, Icon }) => (
-  <S.Link href={url} rel="noopener noreferrer" target="_blank">
+const SocialMediaLink = ({ url, title, Icon }) => (
+  <S.Link href={url} rel="noopener noreferrer" target="_blank" title={title}>
     <Icon />
   </S.Link>
 )
@@ -15,14 +15,17 @@ const FindMe = ({ socialMedias }) => (
     <SocialMediaLink
       url={`https://github.com/${socialMedias.github}`}
       Icon={Github}
+      title="Github"
     />
     <SocialMediaLink
       url={`https://www.linkedin.com/in/${socialMedias.linkedin}`}
       Icon={Linkedin}
+      title="LinkedIn"
     />
     <SocialMediaLink
       url={`https://twitter.com/${socialMedias.twitter}`}
       Icon={Twitter}
+      title="Twitter"
     />
   </S.FindMeWrapper>
 )
