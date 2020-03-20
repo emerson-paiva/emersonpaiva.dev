c596b535f817fd668d678b0bf000d81c122f3f86
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-03-19 22:50:25 -0300

    add manifest configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,13 +24,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Emerson Paiva`,
+        short_name: `Emerson Paiva`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#6d01a5`,
+        theme_color: `#6d01a5`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     {
