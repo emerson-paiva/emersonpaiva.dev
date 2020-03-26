82982432002ab9c85c98965494c5e9b07db61d0e
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-03-26 00:06:53 -0300

    fix(footer): footer style and msg

--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -2,12 +2,14 @@ import styled from 'styled-components'
 
 export const Footer = styled.footer`
   text-align: center;
+  padding: 3rem 0;
   width: 100%;
 `
 
 export const Span = styled.span`
-  color: #757575;
-  font-size: 1.4rem;
+  color: #414141;
+  font-weight: 500;
+  font-size: 1.6rem;
 `
 
 export const Link = styled.a`
