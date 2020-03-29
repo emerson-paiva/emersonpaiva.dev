527c6f7972c7e0814ad91e0023bbf8b660cc1d5b
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-03-28 21:14:59 -0300

    refactor(blog): rename blog page component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,11 +4,11 @@ import Layout from 'components/Layout'
 import BlogList from 'components/BlogList'
 import SEO from 'components/seo'
 
-const IndexPage = () => (
+const BlogPage = () => (
   <Layout>
     <SEO title="Blog" />
     <BlogList />
   </Layout>
 )
 
-export default IndexPage
+export default BlogPage
